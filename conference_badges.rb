d02d0370757acb5e9c18b1f6e26db40252d96246
Brandon Dusch <brandondusch@gmail.com>
2020-03-03 02:09:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -14,6 +14,7 @@ def assign_rooms(attendees)
   room_assignments = []
   attendees.each_with_index{|attendee, index| 
     room_number = index + 1 
-    
+    room_assignments << "Hello, #{attendee}! You'll be assigned to room #{room_number}!"
   }
+  return room_assignments
 end
